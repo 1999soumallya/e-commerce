96ef2a69997f935046a4b5d2b4b8d885f2320d34
1999soumallya <1999soumallya@gmail.com>
2022-10-07 16:06:18 +0530

    Revert "Registractio complete"
    This reverts commit b69fa44a8eccc256a893b1f2e6db75bb68e94273.

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -47,7 +47,7 @@ const Header = () => {
             <LinkContainer to="/cart">
               <Nav.Link>
                 <i className="fas fa-shopping-cart"></i>
-                &nbsp; cart
+                &nbsp; chart
               </Nav.Link>
             </LinkContainer>
             {localStorage.getItem("userInfo") ? (
